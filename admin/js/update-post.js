6768764b25b5770ac1bc87f1210b6df62cdd21a3
Oligaaw <114227397+Oligaaw@users.noreply.github.com>
2022-12-13 15:37:53 +0100

    tag dropdown prefilled-done

--- admin/js/update-post.js
+++ admin/js/update-post.js
@@ -5,31 +5,35 @@ window.onload = function() {
 updatePost()
     async function updatePost() {
         try {
-            const response = await fetch('https://blog-api-assignment.up.railway.app/posts/' + getId.get("id") );
+            const response = await fetch('https://blog-api-assignment.up.railway.app/posts/' + getId.get("id"));
             let blogPost = await response.json()
-            
-                document.getElementById('title').value = blogPost.title;
-                document.getElementById('name').value = blogPost.author;
-                document.getElementById('tags').value = blogPost.tags.join(', ');
-                document.getElementById('content').value = blogPost.content;
+
+            document.getElementById('title').value   = blogPost.title;
+            document.getElementById('name').value    = blogPost.author;
+            document.getElementById('content').value = blogPost.content;
+
+            const blogTags = blogPost.tags;
+      
+            for (tag of document.querySelectorAll("select#tags > option")) {
+                tag.selected = blogTags.indexOf(tag.value) != -1;
+                };
 
         }   catch(error) {
                 console.log(error)
             }
     
     }
-    
     document.getElementById('update-post-form').addEventListener('submit', async function(e) {
         e.preventDefault();
-
+        
         const updateValue = e.target
-
+        
         let updateData = new FormData(updateValue); 
-
+        
         let updateObject = {
             title: updateData.get('title'),
             author: updateData.get('name'),
-            tags: updateData.get('tags'),
+            tags: updateData.getAll('tags'),
             content: updateData.get('content')
         }
 
@@ -37,15 +41,14 @@ updatePost()
             await fetch('https://blog-api-assignment.up.railway.app/posts/' + getId.get("id"), {
                 method: 'PATCH', 
                 headers: {
-                  'Content-Type': 'application/json'
-                },
+                    'Content-Type': 'application/json'},
                 body: JSON.stringify(updateObject)
             })
-    
+            
             location.replace('index.html')
-
+            
         }   catch(error) {
-                console.log(error)
+            console.log(error)
         }
     })
 
